dce8eaadf5652cd18704e3e8a4b0bf3789cce36a
kheiakiyama <kheiakiyama@gmail.com>
2015-09-22 21:51:19 +0900

    yo angular した

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name hipslackApp
+ * @description
+ * # hipslackApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('hipslackApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hipslackApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the hipslackApp
+ */
+angular.module('hipslackApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
