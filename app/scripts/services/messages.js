aea3b7a06c2218726074d4487f805fe2a6fffc14
kheiakiyama <kheiakiyama@gmail.com>
2015-09-23 15:48:15 +0900

    format date

--- app/scripts/services/messages.js
+++ app/scripts/services/messages.js
@@ -10,9 +10,28 @@
 angular.module('hipslackApp')
   .service('Messages', function ($rootScope) {
     // AngularJS will instantiate a singleton by calling "new" on this function
+    var self = this;
     this.messages = [];
     this.set = function(messages, callback) {
-      this.messages = messages;
+      var items = [];
+      messages.forEach(function(val, index) {
+        var date = self._formatDate(val.date);
+        items.push({ 
+          source: val, 
+          date: date, 
+          message: self._formatMessage(val.message)
+        });
+      });
+      this.messages = items;
       callback.call(this, this.messages);
+    };
+    this._formatDate = function(date) {
+      var tmp = new Date(date);
+      var hours = tmp.getHours();
+      var minutes = tmp.getMinutes();
+      return ('00' + hours).slice(-2) + ':' + ('00' + minutes).slice(-2);
+    };
+    this._formatMessage = function(message) {
+      return message;
     };
   });
