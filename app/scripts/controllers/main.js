2fdbb64868ee271437a36d01bf99e19cfbc2959c
kheiakiyama <kheiakiyama@gmail.com>
2015-09-26 16:03:50 +0900

    activeRoom doesn't have same room

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,13 +12,15 @@ angular.module('hipslackApp')
     $scope.show_rooms = false;
     $scope.show_messages = false;
     $scope.activeRooms = [];
+    $scope._activeRoomIds = [];
     $scope.roomsClick = function() {
       $scope.show_rooms = true;
       $scope.show_messages = false;
     };
     $scope.openRoom = function(room) {
       var historyUri = config.backend + '/v2/room/' + room.id + '/history?auth_token=' + config.authkey;
-      if ($scope.activeRooms.indexOf(room) == -1) {
+      if ($scope._activeRoomIds.indexOf(room.id) == -1) {
+        $scope._activeRoomIds.push(room.id);
         $scope.activeRooms.push(room);
       }
       $http.get(historyUri).success(function(data) {
