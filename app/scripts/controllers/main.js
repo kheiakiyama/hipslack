b3d3df3184e5adf08a9feabdb247467041cb781d
kheiakiyama <kheiakiyama@gmail.com>
2015-10-03 14:06:08 +0900

    remove no need flag

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,6 @@
  */
 angular.module('hipslackApp')
   .controller('MainCtrl', function ($scope, $http, $modal, Messages, Rooms) {
-    $scope.show_messages = false;
     $scope.openedRooms = [];
     $scope.roomsClick = function() {
       var roomsModal = $modal.open({
@@ -23,16 +22,10 @@ angular.module('hipslackApp')
       });
     };    
     $scope.menbersClick = function() {
-      $scope.show_rooms = false;
-      $scope.show_messages = false;
-      $scope.show_menbers = true;
     };
     $scope.openRoom = function(room) {
       Rooms.open(room, function() {
         $scope.messages = Messages.messages;
-        $scope.show_rooms = false;
-        $scope.show_messages = true;
-        $scope.show_menbers = false;
         $scope.openedRooms = Rooms.openedItems;
       });
     };
