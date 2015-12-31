1ef5ac44a11631e9f233e49f1270f173b2e144b0
kheiakiyama <kheiakiyama@gmail.com>
2015-12-31 20:13:15 +0900

    fix warning deprecated method

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ app.on('ready', function() {
 
   // ブラウザ(Chromium)の起動, 初期画面のロード
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   installMenu();
   
